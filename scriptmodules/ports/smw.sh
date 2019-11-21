251bb95812b1c11cd1e47cc74619e98b788fadb0
Conn O'Griofa <connogriofa@gmail.com>
2019-11-21 23:35:02 +0000

    smw: update for rpi4 and kms (switch to mmatyas fork)
    * switch to fluffypillows (mmatyas) fork of Super Mario War
    * enable SDL2 build for maximum compatibility with rpi4/kms clients
    * fix atexit crash for kms clients
    * enable vsync by default
    * include level and world editor

--- scriptmodules/ports/smw.sh
+++ scriptmodules/ports/smw.sh
@@ -13,28 +13,45 @@ rp_module_id="smw"
 rp_module_desc="Super Mario War"
 rp_module_licence="GPL http://supermariowar.supersanctuary.net/"
 rp_module_section="opt"
-rp_module_flags="!mali !kms"
+rp_module_flags=""
 
 function depends_smw() {
-    getDepends libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
+    getDepends libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev zlib1g-dev
 }
 
 function sources_smw() {
-    gitPullOrClone "$md_build" https://github.com/HerbFargus/Super-Mario-War.git
+    gitPullOrClone "$md_build" https://github.com/mmatyas/supermariowar
+    applyPatch "$md_data/0001-avoid-atexit.patch"
+    applyPatch "$md_data/0002-enable-vsync.patch"
+    unzip -o data.zip
 }
 
 function build_smw() {
-    ./configure --prefix="$md_inst"
+    local params=(-DUSE_SDL2_LIBS=1 -DDISABLE_DEFAULT_CFLAGS=1)
+    isPlatform "gles" && params+=(-DSDL2_FORCE_GLES=1)
+
+    cmake . -DCMAKE_INSTALL_PREFIX="$md_inst" ${params[@]}
     make clean
     make
+
+    md_ret_require="$md_build/Binaries/Release/$md_id"
 }
 
 function install_smw() {
-    make install
+    md_ret_files=(
+        'data'
+        'Binaries/Release/smw'
+        'Binaries/Release/smw-leveledit'
+        'Binaries/Release/smw-server'
+        'Binaries/Release/smw-worldedit'
+        'README.md'
+    )
 }
 
 function configure_smw() {
-    addPort "$md_id" "smw" "Super Mario War" "$md_inst/smw"
+    addPort "$md_id" "smw" "Super Mario War" "$md_inst/smw $md_inst/data"
+    addPort "$md_id" "smw-level" "Super Mario War - Level Editor" "$md_inst/smw-leveledit $md_inst/data"
+    addPort "$md_id" "smw-world" "Super Mario War - World Editor" "$md_inst/smw-worldedit $md_inst/data"
 
-    moveConfigFile "$home/.smw.options.bin" "$md_conf_root/smw/.smw.options.bin"
+    moveConfigDir "$home/.smw" "$md_conf_root/$md_id"
 }
